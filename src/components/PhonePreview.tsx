a58b87cef66eefb308e63fbcabd91a687dab26e1
Irakli Maisuradze <irakli.maisuradze31.19@gmail.com>
2024-05-14 01:57:05 +0400

    first commit

---
+++ src/components/PhonePreview.tsx
@@ -0,0 +1,73 @@
+"use client";
+
+import { CaseColor } from "@prisma/client";
+import { useEffect, useRef, useState } from "react";
+import { AspectRatio } from "./ui/aspect-ratio";
+import { cn } from "@/lib/utils";
+
+function PhonePreview({
+  croppedImageUrl,
+  color,
+}: {
+  croppedImageUrl: string;
+  color: CaseColor;
+}) {
+  const ref = useRef<HTMLDivElement>(null);
+  const [renderedDimensions, setRenderedDimensions] = useState({
+    height: 0,
+    width: 0,
+  });
+
+  const handleResize = () => {
+    if (!ref.current) return;
+    const { width, height } = ref.current.getBoundingClientRect();
+    setRenderedDimensions({ width, height });
+  };
+
+  useEffect(() => {
+    handleResize();
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, [ref]);
+
+  let caseBackgroundColor = "bg-zinc-900";
+  if (color === "blue") caseBackgroundColor = "bg-blue-950";
+  if (color === "rose") caseBackgroundColor = "bg-rose-950";
+  if (color === "red") caseBackgroundColor = "bg-red-950";
+  if (color === "green") caseBackgroundColor = "bg-green-600";
+  if (color === "orange") caseBackgroundColor = "bg-organge-600";
+
+  return (
+    <AspectRatio ref={ref} ratio={3000 / 2001} className=" relative">
+      <div
+        className=" absolute z-20 scale-[1.0352]"
+        style={{
+          left:
+            renderedDimensions.width / 2 -
+            renderedDimensions.width / (1216 / 121),
+          top: renderedDimensions.height / 6.22,
+        }}
+      >
+        <img
+          width={renderedDimensions.width / (3000 / 637)}
+          alt="mobile_hand"
+          className={cn(
+            "phone-skew relative z-30 rounded-t-[15px] rounded-b-[10px] md:rounded-t-[30px] md:rounded-b-[20px]",
+            caseBackgroundColor
+          )}
+          src={croppedImageUrl}
+        />
+      </div>
+
+      <div className=" relative h-full w-full z-40">
+        <img
+          src="/clearphone.png"
+          alt="photo"
+          className=" pointer-events-none h-full antialiased rounded-md"
+        />
+      </div>
+    </AspectRatio>
+  );
+}
+
+export default PhonePreview;
---
+++ src/config/products.ts
@@ -0,0 +1,12 @@
+export const PRODUCT_PRICES = {
+  material: {
+    silicone: 0,
+    polycarbonate: 35_00,
+  },
+  finish: {
+    smooth: 0,
+    textured: 15_00,
+  },
+} as const;
+
+export const BASE_PRICE = 40_00;
